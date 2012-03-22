2fbb391d22f7a50bb355486349edfeff4db89992
Zach Aysan <zachaysan@gmail.com>
2012-03-22 19:10:30 -0400

    adding nodes with data

--- tests/basic.py
+++ tests/basic.py
@@ -13,4 +13,10 @@ class BasicTest(unittest.TestCase):
         
     def test_add_node(self):
         g = Grafellow()
-        g.add_node('zach', 'person')
+        self.assertTrue(not g.add_node('zach', 'person'))
+        
+    def test_add_node_with_data(self):
+        g = Grafellow(testing=True)
+        attrs = {'languages': ['python', 'ruby', 'others']}
+        self.assertTrue(not g.add_node('zach', 'person', attrs))
+        self.assertTrue(g.get_node('zach','person'))
