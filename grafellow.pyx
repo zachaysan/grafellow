25e7936fbf901759ac8cd235443ea9124105193e
Zach Aysan <zachaysan@gmail.com>
2012-03-22 18:33:19 -0400

    stubbed out the data import

--- grafellow.pyx
+++ grafellow.pyx
@@ -1,4 +1,5 @@
 from pprint import pprint
+import ujson
 
 class Grafellow(object):
     def __init__(self, filename=None):
@@ -7,3 +8,16 @@ class Grafellow(object):
 
     def load_graph_from_file(self, filename):
         pass
+    
+    def handle_json_line(self, json_line):
+        obj = ujson.loads(json_line)
+        action = obj['action']
+        if action == 'add_node':
+            pass
+        elif action == 'delete_node':
+            pass
+        elif action == 'add_edge':
+            pass
+        elif action == 'delete_edge':
+            pass
+        
