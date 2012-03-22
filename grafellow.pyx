2a2f3c2957576073869f146641a7060ab9981c05
Zach Aysan <zachaysan@gmail.com>
2012-03-22 18:14:06 -0400

    added pyx graph, wish me luck

---
+++ grafellow.pyx
@@ -0,0 +1,9 @@
+from pprint import pprint
+
+class Grafellow(object):
+    def __init__(self, filename=None):
+        if filename:
+            self.load_graph_from_file(filename)
+
+    def load_graph_from_file(self, filename):
+        pass
