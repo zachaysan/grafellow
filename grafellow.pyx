7451c419b5ce8323e2b13c5b524a7a566346a312
Zach Aysan <zachaysan@gmail.com>
2012-03-23 12:19:29 -0400

    grafellow can now complete the completed tests. Some of the class variables are temporary and will be backed by a real data store, like tokyo cabinate or something

--- grafellow.pyx
+++ grafellow.pyx
@@ -1,4 +1,5 @@
 from pprint import pprint
+import collections
 import ujson
 
 class Grafellow(object):
@@ -8,11 +9,22 @@ class Grafellow(object):
             self.load_graph_from_file(filename)
         self.make_internal_structures()
 
+    """ This section deals with adding nodes to the graph """
+
     def add_node(self, node, node_type, attr=None):
+        if not attr:
+            attr = {}
         internal_node_id = self.get_node_id(node, node_type)
         if not internal_node_id:
             internal_node_id = self.assign_internal_node_id(node, node_type)
         self.nodes[internal_node_id] = attr
+        return internal_node_id
+    
+    def add_node_if_not_exists(self, node, node_type, attr=None):
+        internal_node_id = self.get_node_id(node, node_type)
+        if not internal_node_id:
+            internal_node_id = self.add_node(node, node_type, attr)
+        return internal_node_id
 
     def get_node(self, node, node_type):
         internal_node_id = self.get_node_id(node, node_type)
@@ -32,17 +44,54 @@ class Grafellow(object):
         """
         return self.internal_node_ids.get((node, node_type), 0)
 
-    def get_internal_node_ids(self, node, node_type):
-        pass
-
     def get_internal_node_ids_with_nbunch(self, nbunch):
         pass
     
     def assign_internal_node_id(self, node, node_type):
         self.last_internal_node_id += 1
         self.internal_node_ids[(node, node_type)] = self.last_internal_node_id
+        self.external_node_type_tuples[self.last_internal_node_id] = (node, node_type)
         return self.last_internal_node_id
     
+    """ This section deals with adding edges to the graph """
+    
+    def add_non_directed_edge(self, node, node_type, node2, node2_type, edge_type, attrs = None):
+        if not attrs:
+            attrs = {}
+        node_internal_id = self.add_node_if_not_exists(node, node_type)
+        node2_internal_id = self.add_node_if_not_exists(node2, node2_type)
+        if node_internal_id not in self.non_directed_adj:
+            self.non_directed_adj[node_internal_id] = {}
+        self.non_directed_adj[node_internal_id][node2_internal_id] = attrs
+        if node2_internal_id not in self.non_directed_adj:
+            self.non_directed_adj[node2_internal_id] = {}
+        self.non_directed_adj[node2_internal_id][node_internal_id] = attrs
+        
+    def node_edges(self, node, node_type, other_node_type=None):
+        node_id = self.get_node_id(node, node_type)
+        if not other_node_type:
+            return self.node_edge_tuples(node_id)
+        else:
+            return self.node_edges_of_type(node_id, other_node_type)
+
+    def node_edges_of_type(self, node_id, other_node_type):
+        pass_function = None
+        if isinstance(other_node_type, collections.Iterable):
+            pass_function = lambda t: t in other_node_type
+        else:
+            pass_function = lambda t: t == other_node_type
+        for node_id, node_type in self.node_edge_tuples(node_id):
+            if pass_function(node_type):
+                yield node_id
+
+    def node_edge_tuples(self, node_id):
+        for internal_adj_node_id in self.internal_edges(node_id):
+            yield self.external_node_type_tuples[internal_adj_node_id]
+                
+    def internal_edges(self, node_id):
+        for node_internal_id in self.non_directed_adj[node_id]:
+            yield node_internal_id
+
     """ This section deals with importing data from an api or file """
 
     def load_graph_from_file(self, filename):
@@ -65,5 +114,7 @@ class Grafellow(object):
     def make_internal_structures(self):
         self.node_types = {}
         self.internal_node_ids = {}
+        self.external_node_type_tuples = {}
         self.last_internal_node_id = 0
         self.nodes = {}
+        self.non_directed_adj = {}
