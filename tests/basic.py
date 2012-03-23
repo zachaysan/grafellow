bc2d54dd2dde86870e2903173fb7be45dbdde732
Zach Aysan <zachaysan@gmail.com>
2012-03-23 12:18:15 -0400

    stubbed out a bunch of edge related tests

--- tests/basic.py
+++ tests/basic.py
@@ -13,10 +13,55 @@ class BasicTest(unittest.TestCase):
         
     def test_add_node(self):
         g = Grafellow()
-        self.assertTrue(not g.add_node('zach', 'person'))
+        node_id_should_be = 1
+        self.assertTrue(g.add_node('zach', 'person') == node_id_should_be)
         
     def test_add_node_with_data(self):
-        g = Grafellow(testing=True)
+        g = Grafellow()
         attrs = {'languages': ['python', 'ruby', 'others']}
-        self.assertTrue(not g.add_node('zach', 'person', attrs))
+        self.assertTrue(g.add_node('zach', 'person', attrs))
         self.assertTrue(g.get_node('zach','person'))
+        
+    def test_add_non_directed_edge(self):
+        g = Grafellow()
+        g.add_non_directed_edge('zach', 'person', 'laura', 'person', edge_type='friends')
+        self.assertTrue(g.get_node('zach','person') == {})
+        self.assertTrue(g.get_node('laura','person') == {})
+        self.assertTrue('laura' in g.node_edges('zach', 'person', other_node_type='person'))
+
+    def test_add_non_directed_edge_with_multiple_edges_and_tuples(self):
+        g = Grafellow()
+        g.add_non_directed_edge('zach', 'person', 'laura', 'person',
+                                edge_type='friends')
+        g.add_non_directed_edge('zach', 'person', 'jaco', 'robot',
+                                edge_type='friends')
+        self.assertTrue(('jaco','robot') in g.node_edges('zach', 'person'))
+        self.assertTrue(('laura','person') in g.node_edges('zach', 'person'))
+
+    def test_retrieving_edge_type(self):
+        pass
+    
+    def test_retrieving_all_edges(self):
+        pass
+
+    def test_retrieving_all_edges_with_attr_data_returned(self):
+        pass
+    
+    def test_adding_directed_edges(self):
+        pass
+
+    def test_retrieving_all_directed_edges_with_attr_data_returned(self):
+        pass
+    
+    def test_retrieving_a_collapsed_list_of_nodes_that_meet_node_type(self):
+        pass
+
+    def test_retrieving_a_collapsed_list_of_nodes_that_meet_edge_type(self):
+        pass
+
+    def test_retrieving_a_collapsed_list_of_nodes_that_meet_both(self):
+        """ since node_type and edge_type may both be important """
+        pass
+
+    def test_retrieving_all_incoming_edges(self):
+        pass
